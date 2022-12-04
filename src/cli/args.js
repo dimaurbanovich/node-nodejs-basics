2b5eba31c9b00031fc379a7838b241fc140874cc
DMITRI <1124004@gmail.com>
2022-12-05 01:32:33 +0300

    completed cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+
+  const result = args.reduce((acc, curr, index) => {
+    return index % 2 === 0
+      ? [...acc, `${curr.replace('--', '')} is`]
+      : [...acc, `${curr},`];
+  }, []);
+
+  console.log(result.join(' '));
 };
 
 parseArgs();
