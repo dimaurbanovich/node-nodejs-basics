d26374cfdc5da330bd03bf1349f6f23c345569d9
DMITRI <1124004@gmail.com>
2022-12-06 00:41:33 +0300

    completed cp and wt tasks

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,29 @@
+import { cpus } from 'os';
+import { Worker, workerData } from 'worker_threads';
+
 const performCalculations = async () => {
-    // Write your code here
+  const cp = cpus();
+  let num = 10;
+
+  const workers = await Promise.allSettled(
+    cp.map(() => {
+      return new Promise((resolve, reject) => {
+        const worker = new Worker('./src/wt/worker.js', {
+          workerData: num++,
+        });
+
+        worker.on('message', (msg) => resolve(msg));
+        worker.on('error', (msg) => reject(msg));
+      });
+    })
+  );
+
+  const results = workers.map(({ status, value }) => ({
+    status: status === 'fulfilled' ? 'resolved' : 'error',
+    data: status === 'fulfilled' ? value : null,
+  }));
+
+  console.log(results);
 };
 
 await performCalculations();
